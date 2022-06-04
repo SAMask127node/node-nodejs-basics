f3dece781e05b36b008a0ff3ebf79073af47572b
Adil_Askar <samask127node@gmail.com>
2022-06-05 05:33:56 +0600

    Fulfil delete.js
    endpoint could be put to env folder

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,16 @@
+import { stat, unlink } from "fs";
+
 export const remove = async () => {
-    // Write your code here 
+  // Write your code here
+  const err_msg = "FS operation failed";
+  const endpoint = "./src/fs/files/fileToRemove.txt";
+
+  stat(endpoint, (err, data) => {
+    if (err) {
+      throw new Error(err_msg);
+    }
+    unlink(endpoint, () => {});
+  });
 };
+
+remove();
