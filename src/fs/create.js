25b2c8c945b9efc1c126c2fd7e8eaa0e1a7ec8da
Adil_Askar <samask127node@gmail.com>
2022-06-05 01:51:02 +0600

    fulfil create.js
    According to nodeJS doc: open/read/write instead of "access"

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,17 @@
+import { readFile, writeFile } from "fs";
+
 export const create = async () => {
-    // Write your code here 
+  // Write your code here
+  const txtFreshFile = "I am fresh and young";
+  const errMsg = "FS operation failed";
+
+  readFile("./src/fs/files/fresh.txt", (err, data) => {
+    if (err) {
+      writeFile("./src/fs/files/fresh.txt", txtFreshFile, () => {});
+    } else {
+      throw new Error(errMsg);
+    }
+  });
 };
+
+create();
