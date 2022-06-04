7141e1f5b3a0dc554f8acbaf10003b17d5572d3c
Adil_Askar <samask127node@gmail.com>
2022-06-05 05:17:30 +0600

    Fulfil rename.js
    fs.promise could be used

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,28 @@
+import * as fs from "fs";
+import * as path from "path";
+
 export const rename = async () => {
-    // Write your code here 
+  // Write your code here
+  const base_dir = "./src/fs/files";
+  const endpoint_name_old = "wrongFilename.txt";
+  const endpoint_name_new = "properFilename.md";
+  const err_msg = "FS operation failed";
+
+  fs.stat(path.join(base_dir, endpoint_name_old), (err, data) => {
+    if (err) {
+      throw new Error(err_msg);
+    }
+    fs.stat(path.join(base_dir, endpoint_name_new), (err, data) => {
+      if (!err) {
+        throw new Error(err_msg);
+      }
+      fs.rename(
+        path.join(base_dir, endpoint_name_old),
+        path.join(base_dir, endpoint_name_new),
+        () => {}
+      );
+    });
+  });
 };
+
+rename();
