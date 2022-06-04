2b7d7a0bf5510240eed8494ed5ccf1ae545c9240
Adil_Askar <samask127node@gmail.com>
2022-06-05 05:53:34 +0600

    Fulfil list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,18 @@
+import { access, readdir } from "fs";
+
 export const list = async () => {
-    // Write your code here 
+  // Write your code here
+  const err_msg = "FS operation failed";
+  const endpoint = "./src/fs/files";
+
+  access(endpoint, (err) => {
+    if (err) {
+      throw new Error(err_msg);
+    }
+    readdir(endpoint, (e, files) => {
+      console.log(files);
+    });
+  });
 };
+
+list();
