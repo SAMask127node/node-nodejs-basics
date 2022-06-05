391ef20254100e90a9826346fb61f799bc32f0c5
Adil_Askar <samask127node@gmail.com>
2022-06-06 05:20:55 +0600

    fulfil wt
    In worker.js error are thrown in specific cases

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,50 @@
+import { isMainThread, Worker, workerData } from "worker_threads";
+import { cpus } from "os";
+
 export const performCalculations = async () => {
-    // Write your code here
+  // Write your code here
+  let workersCreatedOrder = 0;
+  const numOfCpus = cpus().length;
+
+  const runService = async (n) => {
+    try {
+      const result = await new Promise((resolve, reject) => {
+        const worker = new Worker("./src/wt/worker.js", {
+          workerData: { value: n, id: workersCreatedOrder },
+        });
+        workersCreatedOrder++;
+        worker.on("message", resolve);
+        worker.on("error", reject);
+      });
+      resultArray.push({
+        status: "resolved",
+        data: result.result,
+        id: result.id,
+      });
+    } catch (e) {
+      resultArray.push({ status: "error", data: null, id: e.message });
+    }
+  };
+
+  const promisesArray = [];
+  const resultArray = [];
+  for (let index = 0; index < numOfCpus - 1; index++) {
+    let wasInMainThread = false;
+    while (!wasInMainThread) {
+      if (isMainThread) {
+        promisesArray.push(runService(10 + index));
+        wasInMainThread = true;
+      }
+    }
+  }
+
+  await Promise.all(promisesArray);
+  return resultArray
+    .sort((a, b) => a.id - b.id)
+    .map((m) => {
+      delete m.id;
+      return m;
+    });
 };
+
+console.log(await performCalculations());
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,6 +1,17 @@
+import { Worker, parentPort, workerData } from "worker_threads";
 // n should be received from main thread
-export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+export const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 export const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  // This function sends result of nthFibonacci computations to main thread
+  if (workerData?.value) {
+    const result = nthFibonacci(workerData.value);
+    if (workerData.value % 2 === 0) {
+      throw new Error(workerData.id);
+    }
+    parentPort.postMessage({ result: result, id: workerData.id });
+  }
 };
+
+sendResult();
