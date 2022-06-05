c2eb7f0ae967b3e75758a8c8b5b482558c0dd7af
Adil_Askar <samask127node@gmail.com>
2022-06-05 23:28:42 +0600

    Fulfil zip/decompress.js
    pipe from streamAPI

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,16 @@
+import { Gunzip } from "zlib";
+import { createReadStream, createWriteStream } from "fs";
+
 export const decompress = async () => {
-    // Write your code here 
+  // Write your code here
+
+  const gzUnzipTransform = Gunzip();
+  const fileToGunzip = createReadStream("./src/zip/files/archive.gz");
+  const newGunzipedFile = createWriteStream(
+    "./src/zip/files/fileToCompress.txt"
+  );
+
+  fileToGunzip.pipe(gzUnzipTransform).pipe(newGunzipedFile);
 };
+
+decompress();
