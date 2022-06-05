e52a755b94976491bf827d20565e42e1cb240623
Adil_Askar <samask127node@gmail.com>
2022-06-05 23:07:09 +0600

    Fulfil zip/compress.js

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,13 @@
+import { createGzip } from "zlib";
+import { createReadStream, createWriteStream } from "fs";
+
 export const compress = async () => {
-    // Write your code here 
+  // Write your code here
+  const gzTransform = createGzip();
+  const fileToArchive = createReadStream("./src/zip/files/fileToCompress.txt");
+  const newArchivedFile = createWriteStream("./src/zip/files/archive.gz");
+
+  fileToArchive.pipe(gzTransform).pipe(newArchivedFile);
 };
+
+compress();
