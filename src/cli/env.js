7586b2ae472eb44aa048fbee46b8b1b095f5c8c1
Adil_Askar <samask127node@gmail.com>
2022-06-05 14:03:33 +0600

    Fulfil env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,14 @@
 export const parseEnv = () => {
-    // Write your code here 
+  // Write your code here
+  let textToPrint = "";
+  const envKeys = Object.keys(process.env);
+  for (let index = 0; index < envKeys.length; index++) {
+    const env = envKeys[index];
+    if (env.split("_")[0] === "RSS") {
+      textToPrint = `${textToPrint}${env}=${process.env[env]}; `;
+    }
+  }
+  console.log(textToPrint);
 };
+
+parseEnv();
